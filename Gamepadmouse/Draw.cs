14692d2ced95429d1336222b8a4848c460ffe315
Hollowerz <enkorelbf@gmail.com>
2020-12-03 20:17:02 +0100

    Add project files.

---
+++ Gamepadmouse/Draw.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GamepadMouse
+{
+    public partial class Draw : Form
+    {
+        Graphics g;
+        int x = -1;
+        int y = -1;
+        Boolean moving = false;
+        Pen pen;
+
+        public Draw()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+            pen = new Pen(Color.Black, 5);
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            moving = true;
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moving && x != -1 && y != -1)
+            {
+                g.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+            x = -1;
+            y = -1;
+        }
+    }
+}
---
+++ Gamepadmouse/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Windows.Forms;
+
+namespace GamepadMouse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var inputMonitor = new GamepadMouse();
+			inputMonitor.Start();
+			Application.Run(new Draw());
+		
+			
+			
+		}
+	}
+}
